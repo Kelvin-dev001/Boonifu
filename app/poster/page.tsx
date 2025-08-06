fada5caf39296591092dd72e8ef53561e733fad1
Kelvin-dev001 <kelvinoyugi101@gmail.com>
2025-08-06 17:55:14 +0300

    Fix: add missing onClose prop to PosterGeneratorForm

--- app/poster/page.tsx
+++ app/poster/page.tsx
@@ -3,7 +3,7 @@ import PosterGeneratorForm from "../../components/PosterGeneratorForm";
 export default function PosterPage() {
   return (
     <main className="min-h-screen bg-boonifu-light flex items-center justify-center">
-      <PosterGeneratorForm />
+     <PosterGeneratorForm onClose={() => {}} />
     </main>
   );
 }
